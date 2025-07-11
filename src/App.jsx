1dc28498949b9be48152b00fda137c0941fb6da7
Martin Silva <martinsilva510@gmail.com>
2025-07-11 01:11:47 -0300

    files

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { Profesores } from "./components/Profesores";
 import { Horarios } from "./components/Horarios";
 import { Contacto } from "./components/Contacto";
 import { Galeria } from "./components/Galeria";
+
 import "./fonts/bebasFont.css";
-import { Ubicacion } from "./components/ubicacion";
 
 export default function App() {
   return (
@@ -15,7 +15,7 @@ export default function App() {
       <Profesores />
       <Horarios />
       <Contacto />
-      <Ubicacion />
+
       <Galeria />
       <footer className="text-center text-sm text-zinc-400 py-6">
         © 2025 Gimnasio Libertad. Todos los derechos reservados.
